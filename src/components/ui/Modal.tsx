fa60160a5d39a2f0faede1ee1a0725ee3df0f704
Nisarg Jaiswal <nisujaiswal4@gmail.com>
2023-09-17 18:40:57 +0530

    Fucked Up :(

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,6 +1,58 @@
 "use client";
+// import {
+//   Modal,
+//   ModalContent,
+//   ModalHeader,
+//   ModalBody,
+//   ModalFooter,
+//   Button,
+//   useDisclosure,
+// } from "@nextui-org/react";
 import { Button } from "@/components/ui/Button";
 import Dropzone from "../Dropzone";
+
+
+// export default function NextUIModal() {
+//   const { isOpen, onOpen, onOpenChange } = useDisclosure();
+
+//   return (
+//     <>
+//       <Button onPress={onOpen}>Open Modal</Button>
+//       {/* <Button
+//         onPress={onOpen}
+//         variant={"default"}
+//         size={"default"}
+//         className="w-40"
+//       >
+//         Upload
+//       </Button> */}
+//       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
+//         <ModalContent>
+//           {(onClose) => (
+//             <>
+//               <ModalHeader className="flex flex-col gap-1">
+//                 Upload Document
+//               </ModalHeader>
+//               <ModalBody>
+//                 <Dropzone />
+//               </ModalBody>
+//               <ModalFooter>
+//                 <Button color="danger" variant="light" onPress={onClose}>
+//                   Cancel
+//                 </Button>
+//                 <Button color="primary" onPress={onClose}>
+//                   Upload
+//                 </Button>
+//               </ModalFooter>
+//             </>
+//           )}
+//         </ModalContent>
+//       </Modal>
+//     </>
+//   );
+
+
+// }
 import {
   Dialog,
   DialogContent,
